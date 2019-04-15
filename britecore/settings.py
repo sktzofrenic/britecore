e545a21f3168d6a8e5117aa3cbd40394bf3359a9
sktzofrenic <dan@danwins.com>
2019-04-15 07:59:15 -0400

    Remove more unneeded files

--- britecore/settings.py
+++ britecore/settings.py
@@ -14,7 +14,7 @@ env.read_env()
 ENV = env.str('FLASK_ENV', default='production')
 DEBUG = ENV == 'development'
 SQLALCHEMY_DATABASE_URI = env.str('DATABASE_URL')
-SECRET_KEY = env.str('SECRET_KEY')
+SECRET_KEY = env.str('SECRET_KEY', default='not so secret')
 BCRYPT_LOG_ROUNDS = env.int('BCRYPT_LOG_ROUNDS', default=13)
 DEBUG_TB_ENABLED = DEBUG
 DEBUG_TB_INTERCEPT_REDIRECTS = False
